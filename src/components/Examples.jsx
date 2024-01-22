e262bd4e983cfd041d68262d6c7b7a2dd1166edc
Joha Wilson <johawilsonatu@icloud.com>
2024-01-22 19:33:54 +0000

    create new components for work with multiple JSX slots

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import TabButton from "./TabButton.jsx";
+import Tabs from './Tabs.jsx';
 import { EXAMPLES } from '../data.js';
+import Section from './Section.jsx';
 
 
 export default function Examples() {
@@ -13,28 +15,34 @@ export default function Examples() {
     }
     return (
 
-        <section id="examples">
-          <menu>
-            <TabButton isSelected={selectedTopic === 'components'} onSelect={() => handleSelect("components")}>
-              {" "}
-              Components
-            </TabButton>
-            <TabButton isSelected={selectedTopic === 'jsx'} onSelect={() => handleSelect("jsx")}> JSX</TabButton>
-            <TabButton isSelected={selectedTopic === 'props'} onSelect={() => handleSelect("props")}> Props</TabButton>
-            <TabButton isSelected={selectedTopic === 'state'} onSelect={() => handleSelect("state")}> State</TabButton>
-          </menu>
-          {!selectedTopic ? ( <p>Please select a topic.</p>
-          ) : (
-            <div id="tab-content">
-              <h3> {EXAMPLES[selectedTopic].title} </h3>
+        <Section title="Examples" id="examples">
 
-              <p> {EXAMPLES[selectedTopic].description} </p>
+          <Tabs buttons={<> 
+            <TabButton isSelected={selectedTopic === 'components'} onClick={() => handleSelect("components")}>
+                {" "}
+                Components
+              </TabButton>
+              <TabButton isSelected={selectedTopic === 'jsx'} onClick={() => handleSelect("jsx")}> JSX</TabButton>
+              <TabButton isSelected={selectedTopic === 'props'} onClick={() => handleSelect("props")}> Props</TabButton>
+              <TabButton isSelected={selectedTopic === 'state'} onClick={() => handleSelect("state")}> State</TabButton>
+            
+            </>
+          }>
 
-              <pre>
-                <code> {EXAMPLES[selectedTopic].code} </code>
-              </pre>
-            </div>
-          )}
-        </section>
+            {!selectedTopic ? ( <p>Please select a topic.</p>
+            ) : (
+              <div id="tab-content">
+                <h3> {EXAMPLES[selectedTopic].title} </h3>
+
+                <p> {EXAMPLES[selectedTopic].description} </p>
+
+                <pre>
+                  <code> {EXAMPLES[selectedTopic].code} </code>
+                </pre>
+              </div>
+            )}
+
+          </Tabs>
+        </Section>
     );
 }
